df0b587b7cc802ed9661e32e5675695e020c3e5c
lazzaric92 <lazzaric_92@hotmail.it>
2024-08-20 20:25:00 +0200

    added show() in ApiProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,8 +10,8 @@ class ProjectController extends Controller
 {
     public function index(){
         // ! return a JSON as the API response
-        // $projects = Project::all(); // # eager loading
-        $projects = Project::orderBy('date', 'desc')->paginate(25); // <-- lazy loading
+        // $projects = Project::with('user', 'type', 'technologies')->orderBy('date', 'desc')->all(); // # eager loading
+        $projects = Project::with('user', 'type', 'technologies')->orderBy('date', 'desc')->paginate(25); // <-- lazy loading
 
         return response()->json([
             "success" => "true",
@@ -20,6 +20,13 @@ class ProjectController extends Controller
     }
 
     public function show(Project $project){
+        // $project = Project::with('user', 'type', 'technologies')->findOrFail($id); // # using $id
 
+        $project->loadMissing('user', 'type', 'technologies');  // <-- using dependency
+
+        return response()->json([
+            "success" => "true",
+            "results" => $project
+        ]);
     }
 }
